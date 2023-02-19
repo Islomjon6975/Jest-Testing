538d0bd2d61a03b3c7edcf2f543207acb3c1a88b
islomabdulakhatov <islomabdulakhatov@gmail.com>
2023-02-19 19:14:50 +0500

    clicking events

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import Todo from './components/Todo/Todo';
+import { Counter } from './components/counter/counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Todo />
+      {/* <Todo /> */}
+      <Counter />
     </BrowserRouter>
   </React.StrictMode>
 );
